55d0f856787d6b969c31247de06a1359c84627f0
sebastianRaiquenParisi <sebastianrparisi@gmail.com>
2022-03-28 17:17:05 -0300

    tp1

---
+++ tps/src/tp1/ej1/Grupo.java
@@ -0,0 +1,59 @@
+package tp1.ej1;
+
+import java.util.ArrayList;
+
+public class Grupo {
+	private String nombre;
+	private ArrayList<String> integrantes;
+	public Grupo(String nombre) {
+		super();
+		this.setNombre(nombre);
+		this.integrantes = new ArrayList<String>();
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	private void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public ArrayList<String> getIntegrantes() {
+		return integrantes;
+	}
+	
+	public int getCantidadIntegrantes() {
+		return integrantes.size();
+	}
+	
+	public String buscarIntegrante(String nombreIntegrante) {
+		String integranteEncontrado = null;
+		if(obtenerPosicionIntegrante(nombreIntegrante)!=-1) {
+			integranteEncontrado=nombreIntegrante;
+		}
+		return integranteEncontrado;
+	}
+	
+	public void agregarIntegrante(String nombreIntegrante) {
+		if(buscarIntegrante(nombreIntegrante)==null) {
+			integrantes.add(nombreIntegrante);
+		}
+	}
+	
+	public int obtenerPosicionIntegrante(String nombreIntegrante) {
+		int pos = -1;
+		int i = 0;
+		while(i<=integrantes.size() && pos == -1) {
+			if(integrantes.get(i).equals(nombreIntegrante)) {
+				pos =i;
+			}
+			i++;
+		}
+		return pos;
+	}
+	
+	public String obtenerIntegrante(int pos) {
+		return integrantes.get(pos);
+	}
+}
---
+++ tps/src/tp1/ej1/Test.java
@@ -0,0 +1,12 @@
+package tp1.ej1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Grupo grupo = new Grupo("Grupo 3");
+		grupo.agregarIntegrante("Seba");
+		grupo.obtenerIntegrante(0);
+		grupo.obtenerIntegrante(1);
+	}
+
+}
